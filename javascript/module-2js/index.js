bd312ecd686b46e954372903feb8b167d87f2b37
rostyk <zuemir50@gmail.com>
2018-11-12 21:27:04 +0200

    2js_4

--- javascript/module-2js/index.js
+++ javascript/module-2js/index.js
@@ -6,17 +6,15 @@ let total = 0;
 do {
   userInput = prompt("Введите число");
 
-  if (!isNaN(userInput) && userInput !== "" && userInput !== " ") {
+  if (!isNaN(userInput)) {
     numbers.push(+userInput);
   } else {
     alert("Было введено не число, попробуйте еще раз!");
   }
 } while (userInput !== null);
-
 for (let i of numbers) {
   total += i;
 }
-
-if (numbers.length !== 0) {
+if (total > 0) {
   alert(`Общая сумма чисел равна ${total}`);
 }
