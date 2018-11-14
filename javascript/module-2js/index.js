7a943021a50db721f7450d08d784da7d0a0e0416
rostyk <zuemir50@gmail.com>
2018-11-14 16:01:11 +0200

    2js_6

--- javascript/module-2js/index.js
+++ javascript/module-2js/index.js
@@ -9,14 +9,15 @@ do {
   if (!isNaN(userInput) && userInput !== null) {
     numbers.push(+userInput);
   } else if (userInput === null) {
+    break;
   } else {
     alert("Было введено не число, попробуйте еще раз!");
   }
 } while (userInput !== null);
 
-for (i of numbers) {
-  total += i;
-}
 if (numbers.length !== 0) {
+  for (i of numbers) {
+    total += i;
+  }
   alert(`Общая сумма чисел равна ${total}`);
 }
